a54ae8b78a185b9569532ff5d4d0dab302661ab9
Marta Telkamp <martatelkamp@duck.com>
2026-03-05 08:44:58 -0600

    Add CORS preflight support for GitHub Pages and improve API parse error message

--- server.js
+++ server.js
@@ -28,6 +28,17 @@ const twilioClient =
     : null;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  return next();
+});
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
